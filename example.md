5b18c98409e1d72e3967a614278f9edeb2b688a1
ghxm <max.haag@gmx.net>
2022-01-25 18:20:10 +0100

    Initial commit

---
+++ example.md
@@ -0,0 +1,14 @@
+---
+description: Nuts to soup radio.
+location: London, UK
+logo: nts.jpg
+play_time: 24/7
+stream_url:
+- ['#1', https://stream-relay-geo.ntslive.net/stream, online]
+- ['#2', https://stream-relay-geo.ntslive.net/stream2, online]
+support: https://www.nts.live/supporters
+title: NTS
+url: https://www.nts.live
+
+---
+
---
+++ onair.py
@@ -0,0 +1,62 @@
+import configparser
+import glob
+import ruamel.yaml as yaml
+import urllib.request as urllib2
+import re
+import time
+
+config = configparser.ConfigParser()
+config.read('./config.ini')
+
+stations = glob.glob(config['DEFAULT']['stations_dir'] + "/*.md")
+
+def onair(url, mode="code"):
+    if mode=="code":
+        try:
+            req = urllib2.Request(url)
+            req.add_header('User-Agent', 'Mozilla/5.0')
+            conn = urllib2.urlopen(req)
+            try:
+                conn.close()
+            except:
+                pass
+            return True
+        except:
+            return False
+
+def add_to_list(list, index, val):
+    try:
+        list[index] = val
+    except:
+        list.append(val)
+    return(list)
+
+for station in stations:
+    print(time.time())
+    print('File: ' + station)
+    with open(station, "r+") as f:
+
+        file_string = f.read()
+        yaml_string = re.search(r'^stream_url:(?:.)+?\n(?=^[a-z])', file_string, flags=re.DOTALL|re.MULTILINE)
+
+        if yaml_string is None:
+            continue
+        else:
+            yaml_string = yaml_string[0]
+        data = yaml.safe_load(yaml_string)
+
+        for i, stream in enumerate(data['stream_url']):
+            if onair(stream[1]):
+                list = add_to_list(stream, 2, 'online')
+            else:
+                list = add_to_list(stream, 2, 'offline')
+            data['stream_url'][i] = stream
+        yaml_string_updated = yaml.safe_dump(data, allow_unicode=True)
+
+        file_string = file_string.replace(yaml_string, yaml_string_updated)
+
+    with open(station, "w") as f:
+
+        f.write(file_string)
+
+    time.sleep(config['DEFAULT'].getint('wait_between'))

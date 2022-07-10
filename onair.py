217426ebd8099a02a11ffaf76a5831fe4bced801
ghxm <max.haag@gmx.net>
2022-07-10 15:04:41 +0200

    restore silence code

--- onair.py
+++ onair.py
@@ -5,14 +5,41 @@ import urllib.request as urllib2
 import re
 import time
 import os
+import argparse
+from io import BytesIO
+import requests
+from pydub import AudioSegment
+from pydub.silence import detect_silence, detect_nonsilent
+from pydub.utils import which
+
+AudioSegment.converter = which("ffmpeg")
+
+# parse commandline args
+parser = argparse.ArgumentParser()
+parser.add_argument('-o', '--only', nargs='+', help='only check these stations', default=[])
+parser.add_argument('-e', '--exclude', nargs='+', help='exclude these stations', default=[])
+parser.add_argument('-s', '--check-silence', action='store_true', help='check for silence')
+args = parser.parse_args()
+
+
+if args.check_silence:
+    testmode = "silence"
+else:
+    testmode = "stream"
+
 
 config = configparser.ConfigParser()
 config.read(glob.glob(os.path.dirname(os.path.abspath(__file__)) + '/config.ini'))
 
 stations = glob.glob(config['DEFAULT']['stations_dir'] + "/*.md")
 
-def onair(url, mode="code"):
-    if mode=="code":
+if len(args.only) > 0:
+    stations = [station for station in stations if os.path.basename(station).split('.')[0] in args.only]
+if len(args.exclude) > 0:
+    stations = [station for station in stations if os.path.basename(station).split('.')[0] not in args.exclude]
+
+def onair(url, mode="stream"):
+    if mode=="stream":
         try:
             req = urllib2.Request(url)
             req.add_header('User-Agent', 'Mozilla/5.0')
@@ -24,6 +51,45 @@ def onair(url, mode="code"):
             return True
         except:
             return False
+    elif mode=="silence":
+        if onair(url,mode="stream"):
+            # record from stream and check for silence
+
+            starttime = time.time()
+            recording = BytesIO()
+
+            audio_source = 'https://fm.chunt.org/stream'
+            sound = ''
+
+            r = requests.get(url, stream=True)
+
+            # record from stream
+            for block in r.iter_content(1024):
+                recording.write(block)
+                if time.time() > (starttime + 1):
+                    recording.seek(0)
+                    break
+
+            try:
+                sound = AudioSegment.from_file(recording, "mp3")
+            except:
+                try:
+                    sound = AudioSegment.from_file(recording, format="mp4")
+                except:
+                    sound = AudioSegment.from_file(recording)
+
+            silence_check = detect_silence(sound, min_silence_len=900, silence_thresh=-30)
+
+            silence_list = [len(range(silence[0], silence[1])) > 900 for silence in silence_check]
+
+            # if all detected silences ar elonger than 900 miliseconds
+            if len(silence_list) > 0 and all(silence_list):
+                return False
+            else:
+                return True
+        else:
+            return False
+
 
 def add_to_list(list, index, val):
     try:
@@ -47,6 +113,7 @@ for station in stations:
         data = yaml.safe_load(yaml_string)
 
         if 'stream_url' in data.keys() and data.get('stream_url') is not None:
+    
             for i, stream in enumerate(data['stream_url']):
                 if onair(stream[1], mode = testmode):
                     list = add_to_list(stream, 2, 'online')
@@ -55,7 +122,7 @@ for station in stations:
                 data['stream_url'][i] = stream
             yaml_string_updated = yaml.safe_dump(data, allow_unicode=True)
 
-        file_string = file_string.replace(yaml_string, yaml_string_updated)
+            file_string = file_string.replace(yaml_string, yaml_string_updated)
 
     with open(station, "w", encoding="utf8") as f:
 
